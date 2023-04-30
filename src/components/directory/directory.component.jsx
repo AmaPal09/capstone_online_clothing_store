8c1c5f418253e4d723382ea7a0f8f169ff823e3f
Amarja Palekar <amarjapalekar09@gmail.com>
2023-04-29 23:08:07 -0700

    Create directory component
    Create directory component with its component and style files

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,20 @@
+/* directory.component.jsx */
+import './directory.styles.scss'
+import CategoryItem from'../category-item/category-item.component.jsx';
+
+const Directory = ({categories}) => {
+	return(
+		<div className="directory-container">
+            {categories.map((category) => {
+                return (
+                    <CategoryItem key={category.id} category={category} />
+
+                );
+
+            })}
+
+        </div>
+	);
+}
+
+export default Directory;
