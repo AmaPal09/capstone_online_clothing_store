eea322fdaecad801b369a86163b5a454bae42c4e
Amarja Palekar <amarjapalekar09@gmail.com>
2023-05-03 12:03:49 -0700

    Add sign in page
    Add sign in to nav bar and navigate to a sign in page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Routes, Route} from 'react-router-dom';
 
 import Home from './routes/home/home.route.component.jsx';
 import Navigation from './routes/navigation/navigation.route.component.jsx';
-
+import SignIn from './routes/sign-in/sign-in.route.component.jsx';
 
 const Shop = () => {
     return (
@@ -19,6 +19,7 @@ const App =() =>  {
             <Route path='/' element = {<Navigation/>}>
                 <Route index = {true} element={ <Home/>}/>
                 <Route path='shop' element={ <Shop/>}/>
+                <Route path='sign-in' element={ <SignIn/> } />
             </Route>
         </Routes>
     );
---
+++ src/routes/sign-in/sign-in.route.component.jsx
@@ -0,0 +1,11 @@
+/*  sign-in.route.component.jsx */
+
+const SignIn = () => {
+	return(
+		<div>
+			<h1>Lets sign in </h1>
+		</div>
+	);
+};
+
+export default SignIn
