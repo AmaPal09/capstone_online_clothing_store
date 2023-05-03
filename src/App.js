c2ed9e29d7adc0fca0a89d217d83ec722a566834
Amarja Palekar <amarjapalekar09@gmail.com>
2023-05-02 18:13:31 -0700

    Add multiple routes to the webpage

--- src/App.js
+++ src/App.js
@@ -4,12 +4,19 @@
 import { Routes, Route} from 'react-router-dom';
 import Home from './routes/home/home.route.component.jsx';
 
+const Shop = () => {
+    return (
+        <h1>This is the shop page</h1>
+    );
+};
+
 const App =() =>  {
     return(
         <Routes>
             <Route path='/home' element={ <Home/>}/>
+            <Route path='/shop' element={ <Shop/>}/>
         </Routes>
     );
-}
+};
 
 export default App;
