6bac123eedea719b9d00993f8990270a4cd93e5c
Amarja Palekar <amarjapalekar09@gmail.com>
2023-05-03 07:45:32 -0700

    Use react router dom Links
    Use links to navigate to home and shop pages

--- src/routes/navigation/navigation.route.component.jsx
+++ src/routes/navigation/navigation.route.component.jsx
@@ -7,8 +7,15 @@ import { Outlet, Link} from 'react-router-dom';
 const Navigation = () => {
     return (
         <Fragment>
-            <div className='navigation-container'>
-                <h1>I am the navigation bar</h1>
+            <div className='nav-container'>
+                <Link className='logo' to='/'>
+                	<div>Logo</div>
+                </Link>
+                <div className='nav-links-container'>
+                	<Link className='nav-link' to='/shop'>
+                		SHOP
+                	</Link>
+                </div>
             </div>
             <Outlet/>
         </Fragment>
