6caa216c6c245733d7f13630cb2f077e6c34ed1d
Amarja Palekar <amarjapalekar09@gmail.com>
2023-05-05 07:09:00 -0700

    Remove Google redirect sign in button

--- src/routes/sign-in/sign-in.route.component.jsx
+++ src/routes/sign-in/sign-in.route.component.jsx
@@ -1,24 +1,11 @@
 /*  sign-in.route.component.jsx */
 
-import { useEffect } from 'react';
-
-import { getRedirectResult } from 'firebase/auth';
-
 import {
 	signInWithGooglePopup,
-	signInWithGoogleRedirect,
 	createUserDocumentFromAuth,
-	auth,
 } from '../../utils/firebase/firebase.utils.js';
 
 const SignIn = () => {
-	useEffect(() => { async function getResponse() {
-		const response = await getRedirectResult(auth);
-		if (response) {
-			const userDocRef = await createUserDocumentFromAuth(response.user)
-		}
-	} getResponse();
-	}, []);
 
 	const logGoogleUser = async() => {
 		const response = await signInWithGooglePopup();
@@ -31,7 +18,6 @@ const SignIn = () => {
 		<div>
 			<h1>Lets sign in </h1>
 			<button onClick={logGoogleUser}>SignIn with Google Pop</button>
-			<button onClick={signInWithGoogleRedirect}>SignIn with Google Redirect</button>
 		</div>
 	);
 };
