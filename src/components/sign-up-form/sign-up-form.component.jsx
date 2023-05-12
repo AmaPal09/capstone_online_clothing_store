c1bda116269dff32f205daa2dc0c425a2caa3da6
Amarja Palekar <amarjapalekar09@gmail.com>
2023-05-12 16:20:55 -0700

    Add new button component to the sign up button

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils.js';
 
 import FormInput from '../form-input/form-input.component.jsx';
+import Button from '../button/button.component.jsx';
 
 import './sign-up-form.styles.scss';
 
@@ -76,7 +77,7 @@ const SignUpForm = () => {
 					label = "Confirm Password"
 					type = "password" required onChange = {handleChange} name = "comfirmPassword" value = {comfirmPassword}/>
 
-				<button type = "submit">Sign Up</button>
+				<Button type = "submit">Sign Up</Button>
 			</form>
 		</div>
 	);
