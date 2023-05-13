83fd57673d90ca138ceaa89eca9bc6eac256317d
Amarja Palekar <amarjapalekar09@gmail.com>
2023-05-12 22:01:54 -0700

    Remove unused imports

--- src/routes/authentication/authentication.route.component.jsx
+++ src/routes/authentication/authentication.route.component.jsx
@@ -2,13 +2,9 @@
 
 import "./authentication.route.styles.scss";
 
-import {
-	signInWithGooglePopup,
-	createUserDocumentFromAuth,
-} from '../../utils/firebase/firebase.utils.js';
-
 import SignUpForm from '../../components/sign-up-form/sign-up-form.component.jsx';
 import SignInForm from '../../components/sign-in-form/sign-in-form.component.jsx';
+
 
 const Auth = () => {
 	return(
