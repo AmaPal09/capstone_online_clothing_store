89236efd35d3c71544a8e6edc3ee312ceacd7e1d
Amarja Palekar <amarjapalekar09@gmail.com>
2023-05-12 22:01:16 -0700

    Add styles to authentication page

--- src/routes/authentication/authentication.route.component.jsx
+++ src/routes/authentication/authentication.route.component.jsx
@@ -1,5 +1,6 @@
 /*  authentication.route.component.jsx */
 
+import "./authentication.route.styles.scss";
 
 import {
 	signInWithGooglePopup,
@@ -10,11 +11,8 @@ import SignUpForm from '../../components/sign-up-form/sign-up-form.component.jsx
 import SignInForm from '../../components/sign-in-form/sign-in-form.component.jsx';
 
 const Auth = () => {
-
-
 	return(
-		<div>
-			<h1>Lets sign in </h1>
+		<div className="authentication-container">
 			< SignInForm />
 			< SignUpForm />
 		</div>
