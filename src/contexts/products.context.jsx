0f7514f3534628f8e042d4c66109e855d50f457f
Amarja Palekar <amarjapalekar09@gmail.com>
2023-05-25 16:25:32 -0700

    CHNG: Remove code to load products to firebase

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -4,7 +4,7 @@ import { createContext, useState, useEffect } from 'react';
 
 import { addCollectionAndDocuments } from '../utils/firebase/firebase.utils'
 
-import SHOP_DATA from '../shop-data.js';
+// import SHOP_DATA from '../shop-data.js';
 
 
 export const ProductsContext = createContext({
@@ -13,9 +13,9 @@ export const ProductsContext = createContext({
 
 export const ProductsProvider = ({ children }) => {
 	const [products, setProducts] = useState([]);
-    useEffect(() => {
-        addCollectionAndDocuments('categories', SHOP_DATA); 
-    }, []); 
+    // useEffect(() => {
+    //     addCollectionAndDocuments('categories', SHOP_DATA); 
+    // }, []); 
 	const value = {products};
 
 	return(
