a6da29cecf30c8ba43511eedbf05b26c22a79dd5
Amarja Palekar <amarjapalekar09@gmail.com>
2023-05-25 17:37:42 -0700

    CHNG: get product categories from firebase to product context

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -2,7 +2,7 @@
 
 import { createContext, useState, useEffect } from 'react';
 
-import { addCollectionAndDocuments } from '../utils/firebase/firebase.utils'
+import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils'
 
 // import SHOP_DATA from '../shop-data.js';
 
@@ -13,6 +13,14 @@ export const ProductsContext = createContext({
 
 export const ProductsProvider = ({ children }) => {
 	const [products, setProducts] = useState([]);
+    useEffect(() => {
+        const getCategoriesMap = async() => {
+            const categoryMap = await getCategoriesAndDocuments(); 
+            console.log(categoryMap); 
+        }; 
+
+        getCategoriesMap(); 
+    }, []); 
     // useEffect(() => {
     //     addCollectionAndDocuments('categories', SHOP_DATA); 
     // }, []); 
