d4379ee725c986ca1b891d2141591271b77a8483
Amarja Palekar <amarjapalekar09@gmail.com>
2023-05-28 17:54:24 -0700

    CHNG: Show preview of categories

--- src/routes/shop/shop.route.component.jsx
+++ src/routes/shop/shop.route.component.jsx
@@ -1,7 +1,8 @@
 // shop.route.component.jsx
 import { useContext,Fragment } from 'react';
 
-import ProductCard from '../../components/product-card/product-card.component.jsx';
+// import ProductCard from '../../components/product-card/product-card.component.jsx';
+import CategoryPreview from '../../components/category-preview/category-preview.component.jsx';
 
 import { CategoriesContext } from '../../contexts/categories.context';
 
@@ -13,25 +14,14 @@ const Shop = () => {
 	const { categoriesMap } = useContext(CategoriesContext);
 
 	return (
-        <Fragment>
-            {
-                Object.keys(categoriesMap).map((title) => {
-                    return(
-                        <Fragment key={title}>
-                            <h2>{title}</h2>
-                            <div className="products-container">
-                                { categoriesMap[title].map((product) => (
-                                    <ProductCard key={product.id} product={product}>
-                                    </ProductCard>
-                                    )
-                                )}
-                            </div>
-                        </Fragment>
-                    )
-                })
-            }
-        </Fragment>
-	)
+        <div className=" shop-container">
+            {Object.keys(categoriesMap).map((title) => {
+                const products = categoriesMap[title]; 
+
+                return <CategoryPreview key={title} title={title} products={products} /> 
+            })}
+        </div>
+	); 
 }
 
 export default Shop;
