e91a30757138a0f4ab0d5b1674ce9380e9c90bbd
Amarja Palekar <amarjapalekar09@gmail.com>
2023-06-04 16:04:52 -0700

    CHNG: Use react link to navigate to individual category pages

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,5 +1,7 @@
 //category-preview.component.jsx
 
+import { Link } from "react-router-dom";
+
 import ProductCard from "../product-card/product-card.component";
 
 import "./category-preview.styles.scss"; 
@@ -8,7 +10,7 @@ const CategoryPreview = ({title, products}) => {
     return (
         <div className="category-preview-container">
             <h2>
-                <span className="title">{title.toUpperCase()}</span>
+                <Link className="title" to={title}>{title.toUpperCase()}</Link>
             </h2>
             <div className="preview">
                 {   
