369290325064df0d9b3bd192ba8b7005601dd30b
Amarja Palekar <amarjapalekar09@gmail.com>
2023-06-05 13:34:21 -0700

    CHNG: Use custm styled components instead of SCSS

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -4,7 +4,12 @@ import { useContext } from 'react';
 import { CartContext } from '../../contexts/cart.context';
 import Button, {BUTTON_TYPE_CLASSES} from '../button/button.component.jsx';
 
-import './product-card.styles.scss';
+import {
+    ProductCardContainer, 
+    Footer, 
+    BaseSpan, 
+    PriceSpan,
+} from './product-card.styles.jsx';
 
 
 const ProductCard = ({product}) => {
@@ -14,14 +19,14 @@ const ProductCard = ({product}) => {
     const addProductToCart = () => addItemToCart(product);
 
 	return(
-		<div className="product-card-container">
+		<ProductCardContainer>
 			<img src={imageUrl} alt={`${name}`}/>
-			<div className="footer">
-				<span className="name">{name}</span>
-				<span className="price">{price}</span>
-			</div>
+			<Footer>
+				<BaseSpan>{name}</BaseSpan>
+				<PriceSpan>{price}</PriceSpan>
+			</Footer>
 			<Button buttonType={BUTTON_TYPE_CLASSES.inverted} onClick={addProductToCart}>Add to card</Button>
-		</div>
+		</ProductCardContainer>
 	);
 };
 
