3672a308c738bb56928cf01e023e0d001173531e
Amarja Palekar <amarjapalekar09@gmail.com>
2023-06-04 17:57:22 -0700

    CHNG: Custom stylr cart-icon using styled components

---
+++ src/components/cart-icon/cart-icon.styles.jsx
@@ -0,0 +1,27 @@
+// cart-icon.styles.jsx
+
+import styled from 'styled-components'; 
+
+import { ReactComponent as ShoppingBagSVG} from '../../assets/shopping-bag.svg'; 
+
+export const CartIconContainer = styled.div`
+    width: 45px; 
+    height: 45px; 
+    position: relative; 
+    display: flex; 
+    align-items: center; 
+    justify-content: center; 
+    cursor: pointer; 
+`; 
+
+export const ItemCount = styled.span`
+    position: absolute; 
+    font-size: 10px; 
+    font-weight: bold; 
+    bottom: 12px; 
+`; 
+
+export const ShoppingIcon = styled(ShoppingBagSVG)`
+    width: 24px; 
+    height: 24px;
+`; 
