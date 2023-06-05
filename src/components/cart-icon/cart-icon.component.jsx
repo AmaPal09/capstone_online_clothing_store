14ed03bbaeeb7c12bf3101b4b8f04f4777b3bc0c
Amarja Palekar <amarjapalekar09@gmail.com>
2023-06-05 12:47:11 -0700

    DOC: Adjust spacing for readability

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -6,7 +6,11 @@ import { useContext } from 'react';
 
 import { CartContext } from '../../contexts/cart.context';
 
-import {CartIconContainer, ItemCount, ShoppingIcon} from './cart-icon.styles'; 
+import {
+    CartIconContainer, 
+    ItemCount, 
+    ShoppingIcon, 
+} from './cart-icon.styles'; 
 
 const CartIcon = () => {
     const {isCartOpen, setIsCartOpen, cartCount} = useContext(CartContext); 
