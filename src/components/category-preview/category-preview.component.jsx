fe63b6b502d1c6e3345a237c6ffca37584e22a4f
Amarja Palekar <amarjapalekar09@gmail.com>
2023-06-21 17:31:36 -0700

    DOC: Remove commented code

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,7 +1,5 @@
 //category-preview.component.jsx
 
-// import { Link } from "react-router-dom";
-
 import ProductCard from "../product-card/product-card.component";
 
 import {
--- src/components/category-preview/category-preview.styles.jsx
+++ src/components/category-preview/category-preview.styles.jsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 
 import { Link } from 'react-router-dom'
 
+
 export const CategoryPreviewContainer = styled.div`
     display: flex; 
     flex-direction: column; 
