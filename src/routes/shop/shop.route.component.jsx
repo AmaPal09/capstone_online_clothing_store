24ce8060f2869e96333d06c4dbae076b11e4d2c7
Amarja Palekar <amarjapalekar09@gmail.com>
2023-06-21 17:44:05 -0700

    DOC: Remove commented code

--- src/routes/shop/shop.route.component.jsx
+++ src/routes/shop/shop.route.component.jsx
@@ -5,8 +5,6 @@ import { Routes, Route } from 'react-router-dom';
 import CategoriesPreview from '../categories-preview/categories-preview.route.component.jsx';
 import Category from '../category/category.route.component';
 
-// import './shop.route.styles.scss';
-
 
 const Shop = () => {
   return (
