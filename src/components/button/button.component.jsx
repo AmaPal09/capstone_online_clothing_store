3a75d50745dc3da50d2322e1bcfe1e80df125aa1
Amarja Palekar <amarjapalekar09@gmail.com>
2023-06-26 17:32:26 -0700

    DOC: Add necessary comments for readability

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -6,12 +6,13 @@ import {
     InvertedButton, 
 } from './button.styles';
 
+
+
 export const BUTTON_TYPE_CLASSES = {
     base: "base", 
 	google: 'google-sign-in',
 	inverted: 'inverted',
 }
-
 
 const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) => (
     {
@@ -21,6 +22,8 @@ const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) => (
     }[buttonType]
 )
 
+
+//Button component
 const Button = ({ children , buttonType, ...otherProps }) => {
     const CustomButton  = getButton(buttonType); 
 	return (
