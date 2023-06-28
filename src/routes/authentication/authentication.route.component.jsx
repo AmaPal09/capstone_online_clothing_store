ee5a3812e8fc879a0618ddc5deeb594030be47d7
Amarja Palekar <amarjapalekar09@gmail.com>
2023-06-27 21:14:46 -0700

    DOC

--- src/routes/authentication/authentication.route.component.jsx
+++ src/routes/authentication/authentication.route.component.jsx
@@ -8,6 +8,7 @@ import SignUpForm from '../../components/sign-up-form/sign-up-form.component.jsx
 import SignInForm from '../../components/sign-in-form/sign-in-form.component.jsx';
 
 
+//Auth route component
 const Auth = () => {
 	return(
 		<AuthenticationContainer>
