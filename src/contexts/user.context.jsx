6030afdf4946776ae89a7ff45a6d1653b4fa6694
Amarja Palekar <amarjapalekar09@gmail.com>
2023-06-29 21:59:47 -0700

    CHNG: Use reducer in user context

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -19,18 +19,47 @@ export const UserContext = createContext({
 	setCurrentUser: () => null,
 });
 
+export const USER_ACTION_TYPES = {
+    SET_CURRENT_USER: 'SET_CURRENT_USER'
+}
 
 //Reducer function for user context 
 const userReducer = (state, action) => {
+    console.log('dispatch'); 
+    console.log(action); 
     const {type, payload} = action; 
 
+    switch(type) {
+        case USER_ACTION_TYPES.SET_CURRENT_USER: 
+            return{
+                currentUser: payload
+            }
+
+        default: 
+            throw new Error(`Unhandled type ${type} in userReducer`);
+    }
     
-    return 
 } 
+
+const INITIAL_STATE = {
+    currentUser: null
+}
 
 //User context values provider for children
 export const UserProvider = ({ children }) => {
 	// const [currentUser, setCurrentUser] = useState(null);
+    const [state, dispatch] = useReducer(userReducer, INITIAL_STATE) 
+    
+    const {currentUser} = state; 
+    console.log(currentUser);
+    const setCurrentUser = (user) => {
+        dispatch({
+            type: USER_ACTION_TYPES.SET_CURRENT_USER, 
+            // type: 'NOT_HANDLED', 
+            payload: user, 
+        });
+    }
+
 	const value = { currentUser, setCurrentUser };
 
 	useEffect(() => {
