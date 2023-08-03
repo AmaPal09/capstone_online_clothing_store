7cfb62cc1f62422ad9f79a0594d798145a43dcda
Amarja Palekar <amarjapalekar09@gmail.com>
2023-08-03 12:09:58 -0700

    DOC: Remove commented cart provider code

--- src/index.js
+++ src/index.js
@@ -8,8 +8,6 @@ import {Provider} from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// import { CartProvider } from './contexts/cart.context';
-
 import { store } from './store/store';
 
 import './index.scss';
@@ -20,9 +18,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}> 
         <BrowserRouter>
-            {/* <CartProvider> */}
-                <App />
-            {/* </CartProvider> */}
+            <App />
         </BrowserRouter>
     </Provider>
   </React.StrictMode>
