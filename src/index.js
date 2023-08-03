94421e971809d7d21f44eb92ff42722a4a59d976
Amarja Palekar <amarjapalekar09@gmail.com>
2023-08-03 12:40:23 -0700

    FEAR: Use redux persist

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import {Provider} from 'react-redux'; 
 
+import { PersistGate } from 'redux-persist/integration/react'; 
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { store } from './store/store';
+import { store, persistor } from './store/store';
 
 import './index.scss';
 
@@ -17,9 +19,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}> 
+    <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
             <App />
         </BrowserRouter>
+        </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,39 @@
 import {compose, createStore, applyMiddleware} from 'redux';
-import logger from 'redux-logger'; 
+import { persistStore, persistReducer } from 'redux-persist'; 
+import storage from 'redux-persist/lib/storage'; 
+// import logger from 'redux-logger'; 
 
 import { rootReducer } from './root-reducer';
 
-const middleWares = [logger]; 
+const loggerMiddleWare = (store) => (next) => (action) => {
+    if (!action.type) {
+        return next(action);
+    }
+
+    console.log('type: ', action.type);
+    console.log('payload: ', action.payload);
+    console.log('currentState: ', store.getState());
+
+    next(action); 
+
+    console.log('next state: ', store.getState());
+}
+
+const persistConfig = {
+    key: 'root', 
+    storage, 
+    blacklist: ['user'],
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer); 
+
+// const middleWares = [logger]; 
+const middleWares = [loggerMiddleWare]; 
 const composedEnhancers = compose(applyMiddleware(...middleWares)); 
 
 //root-reducer 
 
-export const store = createStore(rootReducer, undefined, composedEnhancers)
+// export const store = createStore(rootReducer, undefined, composedEnhancers)
+export const store = createStore(persistedReducer, undefined, composedEnhancers)
+
+export const persistor = persistStore(store); 
