ba8b44b5e9b2c7cc5416a7c3316bf6b0ed693e0c
Amarja Palekar <amarjapalekar09@gmail.com>
2023-08-04 16:04:54 -0700

    FEAT: Add spinner to categories component

--- src/routes/category/category.route.component.jsx
+++ src/routes/category/category.route.component.jsx
@@ -8,9 +8,10 @@ import {
 import { useSelector} from 'react-redux'; 
 import { useParams } from "react-router-dom"; 
 
-import { selectCategoriesMap } from "../../store/categories/category.selector";
+import { selectCategoriesMap, selectCategoriesIsLoading } from "../../store/categories/category.selector";
 
 import ProductCard from "../../components/product-card/product-card.component";
+import Spinner from "../../components/spinner/spinner.component";
 
 import {
     CategoryContainer, 
@@ -23,6 +24,7 @@ const Category = () => {
     
     const { category } = useParams();  
     const categoriesMap = useSelector(selectCategoriesMap); 
+    const isLoading = useSelector(selectCategoriesIsLoading); 
 
     const [products, setProducts] = useState(categoriesMap[category]); 
 
@@ -33,14 +35,27 @@ const Category = () => {
     return (
         <Fragment>
             <CategoryTitle>{category.toUpperCase()}</CategoryTitle>
-            <CategoryContainer>
+            { isLoading ? (
+                <Spinner/> 
+            ) : (  
+                <CategoryContainer>
                 {   products && 
                     products.map((product) => <ProductCard 
                         key={product.id} 
                         product={product}/> 
                     )
                 }
-            </CategoryContainer>
+                </CategoryContainer>
+            )
+            }
+            {/* <CategoryContainer>
+                {   products && 
+                    products.map((product) => <ProductCard 
+                        key={product.id} 
+                        product={product}/> 
+                    )
+                }
+            </CategoryContainer> */}
         </Fragment>
     ); 
 }; 
