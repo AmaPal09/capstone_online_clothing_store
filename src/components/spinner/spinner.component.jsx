4af7442855b1c4b4c5a090766b0c2f0d293a8c5e
Amarja Palekar <amarjapalekar09@gmail.com>
2023-08-04 16:04:19 -0700

    ERR: Correct console log error

--- src/components/spinner/spinner.component.jsx
+++ src/components/spinner/spinner.component.jsx
@@ -1,10 +1,12 @@
 
 import { SpinnerContainer, SpinnerOverlay } from './spinner.styles'; 
 
-const Spinner = (
-    <SpinnerOverlay>
-        <SpinnerContainer/>
-    </SpinnerOverlay>
-);  
+const Spinner = () => {    
+    return (
+        <SpinnerOverlay>
+            <SpinnerContainer/>
+        </SpinnerOverlay>
+    ); 
+};  
 
 export default Spinner
