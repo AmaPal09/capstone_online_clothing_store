a74388617ac90d34a999ea177810f96a7888639c
Amarja Palekar <amarjapalekar09@gmail.com>
2023-08-25 10:00:29 -0700

    DOC: Remove unused imports

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -3,11 +3,6 @@
 import { useState, } from 'react';
 import { useDispatch } from 'react-redux';
 
-import {
-	signInWithGooglePopup,
-	signInAuthUserWithEmaiAndPassword,
-} from '../../utils/firebase/firebase.utils.js';
-
 import { 
     googleSignInStart, 
     emailSignInStart, 
